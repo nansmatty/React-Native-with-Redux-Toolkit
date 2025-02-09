22a157c4dc1d3573c31e91fd15613fe25191f265
Narayan Maity <narayanmaitysp1997@gmail.com>
2025-02-09 17:52:53 +0530

    task written down

--- src/screens/Project.tsx
+++ src/screens/Project.tsx
@@ -5,6 +5,26 @@ const Projects: React.FC = () => {
   return (
     <View style={styles.sectionContainer}>
       <Text style={styles.sectionTitle}>Projects</Text>
+      <View style={styles.viewBox}>
+        <Text style={styles.homeWork}>
+          1. Try to apply all the basic component that we learned so far.
+        </Text>
+        <Text style={styles.homeWork}>
+          2. Create sub-screens in projects using stack navigation.
+        </Text>
+        <Text style={styles.homeWork}>
+          3. Apply some basic animation and some combination properties from
+          reanimated.
+        </Text>
+        <Text style={styles.homeWork}>
+          4. Integrate third party API in one of the sub-screen and context
+          there for dark theme.
+        </Text>
+        <Text style={styles.homeWork}>
+          5. Create one form in another sub-screen and manage the state using
+          redux
+        </Text>
+      </View>
     </View>
   );
 };
@@ -18,6 +38,22 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
     textAlign: 'center',
+  },
+
+  viewBox: {
+    marginTop: 20,
+    padding: 8,
+  },
+
+  homeWork: {
+    backgroundColor: '#9d174d',
+    padding: 15,
+    fontSize: 14,
+    fontWeight: 'bold',
+    marginVertical: 10,
+    textAlign: 'center',
+    color: 'white',
+    borderRadius: 20,
   },
 });
 
